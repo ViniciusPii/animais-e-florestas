79fcaee995cb76f412c2448b5f5eb7a1b25b058c
Vinicius <vinysales@hotmail.com>
2019-06-18 12:45:52 -0300

    projeto finalizado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Mudar tab ao click
 $('[data-group]').each(function(){
 	var $allTarget = $(this).find('[data-target]'),
 			$allClick = $(this).find('[data-click]'),
@@ -20,6 +21,7 @@ $('[data-group]').each(function(){
 	});
 });
 
+// Scroll suave para link interno
 $('.menu-nav a[href^="#"]').click(function(e){
 	e.preventDefault();
 	var id = $(this).attr('href'),
@@ -30,6 +32,7 @@ $('.menu-nav a[href^="#"]').click(function(e){
 	}, 500);
 });
 
+// Scroll suave para o topo
 $('.logo').click(function(e){
 	e.preventDefault();
 	$('html, body').animate({
@@ -37,6 +40,7 @@ $('.logo').click(function(e){
 	}, 500)
 });
 
+// Mudar para active o menu de acordo com a área
 $('section').each(function(){
 	var height = $(this).height(),
 			offsetTop = $(this).offset().top,
@@ -53,6 +57,68 @@ $('section').each(function(){
 		}
 	});
 });
+
+// Botão do menu mobile
+$('.mobile-btn').click(function(){
+	$(this).toggleClass('active');
+	$('.mobile-menu').toggleClass('active');
+});
+
+// Slider
+function slider(sliderName, velocidade) {
+	var sliderClass = '.' + sliderName,
+			activeClass = 'active',
+			rotate = setInterval(rotateSlide, velocidade);
+	
+	$(sliderClass + ' > :first').addClass(activeClass);
+
+	$(sliderClass).hover(function(){
+		clearInterval(rotate);
+	}, function() {
+		rotate = setInterval(rotateSlide, velocidade);
+	});
+	
+	function rotateSlide() {
+		var activeSlide = $(sliderClass + ' > .' + activeClass),
+				nextSlide = activeSlide.next();
+
+		if(nextSlide.length == 0) {
+			nextSlide = $(sliderClass + ' > :first');
+		}
+		activeSlide.removeClass(activeClass);
+		nextSlide.addClass(activeClass);
+	} 
+}
+
+slider('introducao', 2000);
+
+
+// Animação ao Scroll
+
+var $target = $('[data-anime="scroll"]'),
+		animationClass = 'animate',
+		offset = $(window).height() * 3/4;
+
+function animeScroll() {
+	var documentTop = $(document).scrollTop();
+	
+	$target.each(function(){
+		var itemTop = $(this).offset().top;
+		if (documentTop > itemTop - offset) {
+			$(this).addClass(animationClass);
+		} else {
+			$(this).removeClass(animationClass);
+		}
+	});
+}
+
+animeScroll();
+
+$(document).scroll(function(){
+	animeScroll();
+});
+
+
 
 
 
